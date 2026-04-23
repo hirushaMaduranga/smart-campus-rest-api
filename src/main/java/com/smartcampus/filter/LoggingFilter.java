b138bb9b859cf6486eeb3d7fd3f8b8bb7bb8c608
Hirusha Maduranga <hirushapathum2003@gmail.com>
2026-04-24 00:30:13 +0530

    set

---
+++ src/main/java/com/smartcampus/filter/LoggingFilter.java
@@ -0,0 +1,26 @@
+package com.smartcampus.filter;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class LoggingFilter implements ContainerRequestFilter, ContainerResponseFilter {
+    private static final Logger LOGGER = Logger.getLogger(LoggingFilter.class.getName());
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        LOGGER.info("REQUEST " + requestContext.getMethod() + " " + requestContext.getUriInfo().getRequestUri());
+    }
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        LOGGER.info("RESPONSE " + requestContext.getMethod() + " " + requestContext.getUriInfo().getRequestUri()
+            + " -> " + responseContext.getStatus());
+    }
+}
