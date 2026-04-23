32662188289ef221d038c4be19203e4931fdee65
Hirusha Maduranga <hirushapathum2003@gmail.com>
2026-04-24 00:31:27 +0530

    DiscoveryResource.java

---
+++ src/main/java/com/smartcampus/resource/DiscoveryResource.java
@@ -0,0 +1,30 @@
+package com.smartcampus.resource;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("")
+@Produces(MediaType.APPLICATION_JSON)
+public class DiscoveryResource {
+
+    @GET
+    public Map<String, Object> getApiInfo() {
+        Map<String, Object> response = new LinkedHashMap<>();
+
+        response.put("name", "Smart Campus API");
+        response.put("version", "v1");
+        response.put("adminContact", "admin@smartcampus.local");
+
+        Map<String, String> resources = new LinkedHashMap<>();
+        resources.put("rooms", "/api/v1/rooms");
+        resources.put("sensors", "/api/v1/sensors");
+
+        response.put("resources", resources);
+        return response;
+    }
+}
